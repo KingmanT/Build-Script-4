f12a023dbacaecac70df1446e139295c567f4763
Kingman <Kingman.Tam@gmail.com>
2022-09-12 21:59:53 -0400

    Added API requests from google and sunrise-sunset based on user input

--- build_script_4v1.py
+++ build_script_4v1.py
@@ -1,4 +1,24 @@
-import subprocess
-import urllib.request
-import json
+#!/usr/bin/python3
 
+import json
+import sys
+import requests
+
+latlong = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + (sys.argv[1]) + '&key=AIzaSyAa1469a_qPfqkSMEjK3OQ8Md7u4Ap10b4')
+# latlong = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=new york&key=AIzaSyAa1469a_qPfqkSMEjK3OQ8Md7u4Ap10b4')
+# print(latlong.json())
+latitude = latlong.json()['results'][0]['geometry']['location']['lat']
+longitude = latlong.json()['results'][0]['geometry']['location']['lng']
+print(latitude)
+print(longitude)
+
+# print(sys.argv)
+# print(sys.arg[1])
+# suninfo = requests.get('https://api.sunrise-sunset.org/json?lat=40.7127753,&lng=-74.0059728&date=9/12/22')
+# suninfo = requests.get('https://api.sunrise-sunset.org/json?lat=' + str(latitude) + ',&lng=' + str(longitude) + '&date=9/13/22')
+suninfo = requests.get('https://api.sunrise-sunset.org/json?lat=' + str(latitude) + ',&lng=' + str(longitude) + '&date=' + str(sys.argv[2]))
+# print(suninfo.json())
+sunrise = suninfo.json()['results']['sunrise']
+sunset = suninfo.json()['results']['sunset']
+print(sunrise)
+print(sunset)
--- intro.sh
+++ intro.sh
@@ -4,9 +4,26 @@
 # and then use that input to get the sunrise/sunset information on that time zone.
 
 echo "Welcome to the sunrise/sunset calculator"
+echo "------------------------------------------"
 read -p "Please press enter to continue or type 'exit' to quit program " ans1
+echo "------------------------------------------"
 if [[ $ans1 = "exit" ]]; then
-echo "Exiting program"
-exit 0
+    echo "Exiting program"
+    echo "------------------------------------------"
+    exit 0
 fi
-echo "so far so good"
+echo " "
+echo "This program will allow you to check the time that"
+echo "the sun will set and rise based on the location you enter"
+echo " "
+echo "------------------------------------------"
+echo "Please enter the time zone or city of that you would like to get information about"
+echo "------------------------------------------"
+read inputlocation
+echo "------------------------------------------"
+echo "Please enter the date that you would like to get information on"
+echo "------------------------------------------"
+read inputdate
+
+python3 build_script_4v1.py "$inputlocation" "$inputdate"
+
